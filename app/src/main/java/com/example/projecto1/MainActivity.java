739449ebadbfedfdc15553667cc204f3bf68ee62
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-14 21:49:56 -0400

    Creación de diseño con nombre, apellido y correo.

--- app/src/main/java/com/example/projecto1/MainActivity.java
+++ app/src/main/java/com/example/projecto1/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.projecto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "Estamos en el metodo onCreate");
         setContentView(R.layout.activity_main);
     }
+
 }
